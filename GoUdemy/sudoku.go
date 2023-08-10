3ff08714287ba1292b46fed459b2846a112545b1
ktchk <16212495+ktchk@users.noreply.github.com>
2023-08-10 19:29:21 +0300

    no message

---
+++ GoUdemy/sudoku.go
@@ -0,0 +1,33 @@
+package main
+
+/*
+
+-------------------------
+| 7 0 8 | 0 0 1 | 4 0 0 |
+| 3 0 0 | 4 7 0 | 0 0 0 |
+| 0 9 4 | 5 0 0 | 0 7 6 |
+-------------------------
+| 2 7 0 | 6 0 8 | 0 0 0 |
+| 6 0 3 | 0 0 0 | 7 0 2 |
+| 0 0 0 | 7 0 0 | 1 6 0 |
+-------------------------
+| 0 0 7 | 0 5 4 | 0 0 1 |
+| 4 1 5 | 0 0 0 | 8 2 7 |
+| 0 0 0 | 0 0 7 | 0 0 4 |
+-------------------------
+
+*/
+
+/* Plan:
+1. [][]int to store the puzzle
+2. display the puzzle func
+3. main with func that will find index of 0 and if not = puzzle is solved
+4. func than will check the possibility of entering (1-9) in that index in table
+5. ...
+6. profit!
+
+*/
+
+func main() {
+
+}
